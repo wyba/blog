d55154e7633922423ae5a635ca27d2f6a885a97f
wei <1070096495@qq.com>
2021-12-03 17:08:42 +0800

    重新部署吧

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # blog
 个人博客
+
+删除main、gh-page分支，继续部署
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+const head = require('./config/head.js');
+const plugins = require('./config/plugins.js');
+const themeConfig = require('./config/themeConfig.js');
+
+module.exports = {
+  theme: 'vdoing', // 使用npm包主题
+  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+
+  title: "wyb's blog",
+  description: '主流技术博客,简洁至上,专注全栈学习与总结。',
+  base: '/blog/', // 格式：'/<仓库名>/'， 默认'/'
+  markdown: {
+    lineNumbers: true, // 代码行号
+  },
+
+  head,
+  plugins,
+  themeConfig,
+}
