0d0b53bd353b28207fc9de3f009dbd7a8ebef547
wei <1070096495@qq.com>
2021-12-20 17:58:01 +0800

    如此艰难的更新

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -54,7 +54,7 @@ module.exports = {
     // 博主信息，显示在首页侧边栏
     avatar: 'https://cdn.jsdelivr.net/gh/wyba/image_store/blog/lufei.jpeg',
     name: 'wyb',
-    slogan: '疯狂copy',
+    slogan: '吾日三省吾身',
   },
   social: {
     // 社交图标，显示于博主信息栏和页脚栏
